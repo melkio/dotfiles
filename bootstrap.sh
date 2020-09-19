3b7f6ee1f12d40b975dda3aa8cd5e5815ed2d2ea
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2020-09-19 10:57:34 +0200

    add bootstrap script to link dotfiles

---
+++ bootstrap.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+#
+# bootstrap installs things.
+
+DOTFILES_ROOT=$(pwd -P)
+
+set -e
+
+echo ''
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+link_file () {
+  local src=$1 dst=$2
+  local skip="false"
+
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+  then
+    local currentSrc="$(readlink $dst)"
+
+    if [ "$currentSrc" = "$src" ]
+    then
+      skip="true";
+    else
+      mv "$dst" "${dst}.backup"
+      success "moved $dst to ${dst}.backup"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]
+  then
+    sudo ln -s "$1" "$2"
+    success "linked $1 to $2"
+  else  
+    success "skipped $src"
+  fi
+}
+
+link_file "$DOTFILES_ROOT/bash/.bashrc" "$HOME/.bashrc"
+link_file "$DOTFILES_ROOT/git/.gitconfig" "$HOME/.gitconfig"
+link_file "$DOTFILES_ROOT/i3/config" "$HOME/.config/i3/config"
+
+echo ''
+echo '  All installed!'
