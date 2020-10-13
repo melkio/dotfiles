776f168b6dba9f9192f8126261c875bd20c0ea5d
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2020-10-13 16:23:09 +0200

    change i3 super key (win -> alt)

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,11 +53,12 @@ link_file () {
   fi
 }
 
-link_file "$DOTFILES_ROOT/bash/.bashrc" "$HOME/.bashrc"
+#link_file "$DOTFILES_ROOT/bash/.bashrc" "$HOME/.bashrc"
 link_file "$DOTFILES_ROOT/git/.gitconfig" "$HOME/.gitconfig"
-link_file "$DOTFILES_ROOT/i3/config" "$HOME/.config/i3/config"
-link_file "$DOTFILES_ROOT/i3/compton.conf" "$HOME/.config/i3/compton.conf"
-link_file "$DOTFILES_ROOT/xorg/.xsession" "$HOME/.xsession"
+link_file "$DOTFILES_ROOT/regolith/Xresources" "$HOME/.config/regolith/Xresources"
+#link_file "$DOTFILES_ROOT/i3/config" "$HOME/.config/i3/config"
+#link_file "$DOTFILES_ROOT/i3/compton.conf" "$HOME/.config/i3/compton.conf"
+#link_file "$DOTFILES_ROOT/xorg/.xsession" "$HOME/.xsession"
 
 echo ''
 echo '  All installed!'
