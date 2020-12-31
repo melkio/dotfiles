08bb275680a052042fe7bc5bff34927a0631b1cd
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2020-12-31 12:12:59 +0100

    remove unused code

--- bootstrap.sh
+++ bootstrap.sh
@@ -61,9 +61,6 @@ link "$DOTFILES_ROOT/regolith/Xresources" "$HOME/.config/regolith/Xresources"
 link "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/01_setup" "$HOME/.config/regolith/i3xrocks/conf.d/01_setup"
 link "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/80_rofication" "$HOME/.config/regolith/i3xrocks/conf.d/80_rofication"
 link "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/90_time" "$HOME/.config/regolith/i3xrocks/conf.d/90_time"
-#link "$DOTFILES_ROOT/i3/config" "$HOME/.config/i3/config"
-#link "$DOTFILES_ROOT/i3/compton.conf" "$HOME/.config/i3/compton.conf"
-#link "$DOTFILES_ROOT/xorg/.xsession" "$HOME/.xsession"
 
 echo ''
 echo '  All installed!'
