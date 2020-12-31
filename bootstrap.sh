7424664180f3b9ce57d67b0e74acec14c73ef227
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2020-12-31 15:31:40 +0100

    refactoring: remove duplicated code

--- bootstrap.sh
+++ bootstrap.sh
@@ -57,6 +57,12 @@ link "$DOTFILES_ROOT/bin" "$HOME/.local/bin"
 link "$DOTFILES_ROOT/bash/bashrc" "$HOME/.bashrc"
 link "$DOTFILES_ROOT/bash/bash_profile" "$HOME/.bash_profile"
 link "$DOTFILES_ROOT/git/gitconfig" "$HOME/.gitconfig"
+
+link "$DOTFILES_ROOT/asdf/install" "$HOME/.local/source/asdf_install"
+link "$DOTFILES_ROOT/asdf/alias" "$HOME/.local/source/asdf_alias"
+link "$DOTFILES_ROOT/git/alias" "$HOME/.local/source/git_alias"
+link "$DOTFILES_ROOT/dotnet/alias" "$HOME/.local/source/dotnet_alias"
+
 link "$DOTFILES_ROOT/regolith/Xresources" "$HOME/.config/regolith/Xresources"
 link "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/01_setup" "$HOME/.config/regolith/i3xrocks/conf.d/01_setup"
 link "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/80_rofication" "$HOME/.config/regolith/i3xrocks/conf.d/80_rofication"
