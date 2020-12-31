53acc1c6bba5656a9b856ff3b7c3d6bcdc6577cc
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2020-12-31 12:12:42 +0100

    lin bin folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,7 +26,7 @@ fail () {
   exit
 }
 
-link_file () {
+link () {
   local src=$1 dst=$2
   local skip="false"
 
@@ -46,23 +46,24 @@ link_file () {
   if [ "$skip" != "true" ]
   then
     mkdir -p $(dirname "$dst")
-    ln -s "$1" "$2"
-    success "linked $1 to $2"
+    ln -s "$src" "$dst"
+    success "linked $src to $dst"
   else  
     success "skipped $src"
   fi
 }
 
-link_file "$DOTFILES_ROOT/bash/bashrc" "$HOME/.bashrc"
-link_file "$DOTFILES_ROOT/bash/bash_profile" "$HOME/.bash_profile"
-link_file "$DOTFILES_ROOT/git/gitconfig" "$HOME/.gitconfig"
-link_file "$DOTFILES_ROOT/regolith/Xresources" "$HOME/.config/regolith/Xresources"
-link_file "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/01_setup" "$HOME/.config/regolith/i3xrocks/conf.d/01_setup"
-link_file "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/80_rofication" "$HOME/.config/regolith/i3xrocks/conf.d/80_rofication"
-link_file "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/90_time" "$HOME/.config/regolith/i3xrocks/conf.d/90_time"
-#link_file "$DOTFILES_ROOT/i3/config" "$HOME/.config/i3/config"
-#link_file "$DOTFILES_ROOT/i3/compton.conf" "$HOME/.config/i3/compton.conf"
-#link_file "$DOTFILES_ROOT/xorg/.xsession" "$HOME/.xsession"
+link "$DOTFILES_ROOT/bin" "$HOME/.local/bin"
+link "$DOTFILES_ROOT/bash/bashrc" "$HOME/.bashrc"
+link "$DOTFILES_ROOT/bash/bash_profile" "$HOME/.bash_profile"
+link "$DOTFILES_ROOT/git/gitconfig" "$HOME/.gitconfig"
+link "$DOTFILES_ROOT/regolith/Xresources" "$HOME/.config/regolith/Xresources"
+link "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/01_setup" "$HOME/.config/regolith/i3xrocks/conf.d/01_setup"
+link "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/80_rofication" "$HOME/.config/regolith/i3xrocks/conf.d/80_rofication"
+link "$DOTFILES_ROOT/regolith/i3xrocks/conf.d/90_time" "$HOME/.config/regolith/i3xrocks/conf.d/90_time"
+#link "$DOTFILES_ROOT/i3/config" "$HOME/.config/i3/config"
+#link "$DOTFILES_ROOT/i3/compton.conf" "$HOME/.config/i3/compton.conf"
+#link "$DOTFILES_ROOT/xorg/.xsession" "$HOME/.xsession"
 
 echo ''
 echo '  All installed!'
