92cdbdb35ccf85305bda94b1f3f88d190871e53f
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2021-05-30 11:19:31 +0200

    remove unused vscode extensions and add new ones

--- vscode/configure.sh
+++ vscode/configure.sh
@@ -3,9 +3,10 @@
 extensions=(
     "PKief.material-icon-theme"
     "ms-dotnettools.csharp"
-    "kreativ-software.csharpextensions"
-    "Ionide.Ionide-fsharp"
-    "ms-azuretools.vscode-docker"
+    "jchannon.csharpextensions"
+    "josefpihrt-vscode.roslynator"
+    "hashicorp.terraform"
+    "Elmtooling.elm-ls-vscode"
 )
 
 for ext in "${extensions[@]}"
